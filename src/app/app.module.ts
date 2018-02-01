290fd04b7ff6f8afef00eec38a6bb81df697be4e
antenorr <roudyantenor@gmail.com>
2018-02-01 14:12:29 -0500

    Add the sidebar and main sidebar content section material componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,26 @@ import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { ToolTopComponent } from './tool-top/tool-top.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolTopComponent
+    ToolTopComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]

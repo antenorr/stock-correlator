566b9be1011077b93e25450ab3576a95b04e5760
antenorr <roudyantenor@gmail.com>
2018-02-01 23:41:33 -0500

    removed the sidebar material feature for it was unecessary given the scope of what we wish to accomplish and simpler method was taken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 import { AppComponent } from './app.component';
 import { ToolTopComponent } from './tool-top/tool-top.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolTopComponent,
-    SidenavComponent
+    ToolTopComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,11 @@ import { SidenavComponent } from './sidenav/sidenav.component';
     MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

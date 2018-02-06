0e968c5b04945e37210dad938e993cae2d3cb82c
antenorr <roudyantenor@gmail.com>
2018-02-05 20:02:40 -0500

    Gather the values of base security, base security start and end dates and the values of the correlator security - it's start date and end date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,28 @@ import { Component } from '@angular/core';
 
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  // selected = 'option2';
-  selectedValue: string = " ";
-  selected: string;
+  
+
   answer: string;
+
+// *** NOTES *** the two securities to be set in order to correlate
+  baseSelection: string ;
+  correlationSelection: string;
+
+  // *** NOTES *** the start and end dates for each of the two securities in order to correlate
+  baseStartDate: string;
+  baseEndDate: string;
+  correlatorStartDate: string;
+  correlatorEndDate: string;
+
+
 
   stocks = [
     {value: ' MMM', viewValue: '3M'},
@@ -27,8 +38,10 @@ export class AppComponent {
   ];
 
   showValues() {
-    this.answer = this.selected;
-    console.log(this.answer);
+    this.answer = this.correlationSelection;
+    //console.log(this.answer);
+    // console.log(this.baseSelection);
+    console.log(this.baseStartDate, this.baseEndDate);
     this.answer = null;
   }
  
